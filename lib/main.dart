d30c5b5d72fa5607965424f1ebab3fa908ce6957
sharifc2016 <sharifc2016@gmail.com>
2021-09-06 09:36:41 +0600

    init quiz success/error count icons

--- lib/main.dart
+++ lib/main.dart
@@ -87,6 +87,18 @@ class _QuizPageState extends State<QuizPage> {
             ),
           ),
         ),
+        Row(
+          children: [
+            Icon(
+              Icons.check,
+              color: Colors.green,
+            ),
+            Icon(
+              Icons.close,
+              color: Colors.red,
+            ),
+          ],
+        ),
         //TODO: Add a Row here as your score keeper
       ],
     );
